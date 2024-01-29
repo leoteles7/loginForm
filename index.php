fa4b2019ce7012913ee3653b4eba7fabdd1d506b
mac pagprime <macpagprime@MacBook-Pro-de-mac.local>
2024-01-29 14:46:05 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div>
+        <h1>Login</h1>
+        <input type="text" placeholder="Email">
+        <br><br>
+        <input type="password" placeholder="Senha">
+        <br><br>
+        <button>Entrar</button>
+    </div>
+</body>
+</html>
